0e0457cc8a1c3373957e0d64f100310d8c3511b5
LinoRino <omurakenshin@gmail.com>
2022-10-14 10:12:58 +0900

    add: views on blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+  return HttpResponse("Hello, world.")
+def login_page(request):
+  return HttpResponse("Login")
