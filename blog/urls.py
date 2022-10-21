c645cfce74c3e45dda060483d19eed0aa9733a6b
LinoRino <omurakenshin@gmail.com>
2022-10-21 11:00:00 +0900

    Added: login

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from blog import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail', views.detail, name='detail'),
+    path("register", views.AccountCreateView.as_view(), name="register")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.models import User
 
+class AccountCreateView(View):
+    def get(self, request):
+        return render(request, "blog/register.html")
+    
+    # post を追加
+    def post(self, request):
+        # ユーザー情報を保存する
+        User.objects.create_user(
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        # 登録完了画面を表示する
+        return render(request, "blog/register_success.html")
 def index(request):
     return HttpResponse("Hello, world.")
 def detail(request):
