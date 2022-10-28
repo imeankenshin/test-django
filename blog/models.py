14055824815ca6782f422c962ffef51074e29952
LinoRino <omurakenshin@gmail.com>
2022-10-28 11:13:30 +0900

    Added: models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from distutils.text_file import TextFile
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import CharField
 
 # Create your models here.
+class Article(models.Model):
+  title= models.CharField(max_length=200)
+  body = models.TextField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
