1eec6861496d5e1f15c96dd23795bf306beaf704
LinoRino <omurakenshin@gmail.com>
2022-12-02 10:43:17 +0900

    Add: API

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path("mypage/new-article", views.ArticleCreateView.as_view(), name="mypage-new-article"),
     path("mypage/articles", views.MypageArticleView.as_view(), name="mypage-articles"),
     path("articles", views.ArticleListView.as_view(), name="articles"),
-    path("articles/<id>", views.ArticleView.as_view(), name="article")
+    path("articles/<id>", views.ArticleView.as_view(), name="article"),
+    path("api/articles", views.ArticleApiView.as_view(), name="api")
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from blog.models import Article
-from blog.quitta import QuittaAPI
+from blog.quitta import QiitaApiClient
+from django.http import JsonResponse
+import json
 
 class AccountLoginView(LoginView):
     """ログインページのテンプレート"""
@@ -39,13 +41,24 @@ def index(request):
     articles = Article.objects.all()
 
     #QuittaのAPIを読み込む
-    quitta = QuittaAPI()
-    quitta.get_django_artic()
+    qitta_api = QiitaApiClient()
+    
+    # qiita の API がエラーになったかどうか表すフラグ
+    is_qiita_error = False
+    # 記事一覧を初期化しておく
+    qitta_articles = []
+    
+    try:
+        qitta_articles = qitta_api.get_django_articles()
+    except RuntimeError:
+        is_qiita_error = True
     
     # こうすることで、article 変数をテンプレートにわたす事ができる
     # {テンプレート上での変数名: 渡す変数}
+    print(qitta_articles)
     return render(request, "blog/index.html", {
-        "articles": articles
+        "articles": articles,
+        "qiita_articles": qitta_articles,
     })
 
 class MypageView(LoginRequiredMixin, View):
@@ -94,4 +107,37 @@ class ArticleView(View):
         article = Article.objects.get(id=id)
         return render(request,"blog/article.html", {
             "article":article
+        })
+        
+class ArticleApiView(View):
+    def get(self, req):
+        #DBからArticleを取得
+        articles = Article.objects.all()
+        
+        dict_articles = []
+        for article in articles:
+            dict_article = {
+                "id":article.id,
+                "title": article.title,
+                "body": article.body,
+            }
+            dict_articles.append(dict_article)
+            
+        json = {
+            "articles": dict_article,
+        }
+        return JsonResponse(json)
+    
+    #TODO: 記事を投稿するAPIを作る。 Cookie = csrftoken=bQN8DCuGVhyo1l9ghxNSChikNcTstEKE; Sessionid=9uiippo8ltmb93bvq1noqr80flu6o45e;
+    def post(self,request):
+        json_dict = json.loads(request.body)
+        article = Article(
+            title=json_dict["title"],
+            body=json_dict["body"],
+            # user には、現在ログイン中のユーザーをセットする
+            user=request.user,
+        )
+        article.save()
+        return JsonResponse({
+            "message": "記事の投稿に成功しました。"
         })
